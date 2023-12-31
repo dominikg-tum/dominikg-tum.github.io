17d7fe724f7d02568647df7abb744c58e436b903
dominikg-tum <dgarstenauer0@gmail.com>
2023-12-31 17:13:48 +0100

    added main page beginning text & styling

--- portfolio/src/components/Main.svelte
+++ portfolio/src/components/Main.svelte
@@ -1,1 +1,18 @@
-Main
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+	<section id="introPage" class="grid grid-cols-1 lg:grid-cols2 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+			<h2 class="font-semibold text-4xl sm:text-5xö md:text-6xl lg:text-7xl">
+				Hi! I'm <span class="poppins text-violet-400">Dominik</span> Garstenauer, 
+                <br /> a B.Sc. in <span class="poppins text-violet-400">Computer Science :)</span> 
+			</h2>
+            <p class="text-base sm:text-lg md:text-xl lg:text-2xl">
+                I have obtained my degree at the <span class="poppins text-violet-400"> <a href="https://www.tum.de" target="_blank"> Techincal University of Munich (TUM)</a></span> in 2023
+                <br /> and have studied a <span class="poppins text-violet-400"> wide range </span> of super interesting topics like 
+                <br /> <span class="poppins text-violet-400"> ML & DL, SE, Robotics, MbSE,  </span> and much more ... 
+            </p>
+		</div>
+	</section>
+</main>
