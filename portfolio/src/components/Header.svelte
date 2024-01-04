907a8bc7edae93f353ba224e0cb18456bfc6c026
dominikg-tum <dgarstenauer0@gmail.com>
2024-01-04 14:49:31 +0100

    index html

--- portfolio/src/components/Header.svelte
+++ portfolio/src/components/Header.svelte
@@ -28,15 +28,15 @@
 				<p>{tab.name}</p>
 			</a>
 		{/each}
-		<a
-			href="https://www.linkedin.com/in/dominik-garstenauer/"
-			target="_blank"
-			class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-		>
-			<div
-				class="absolute top-0 right-full w-full h-full bg-gradient-to-r from-violet-400 to-violet-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200"
-			/>
-			<h4 class="relative z-9">Get in touch</h4>
-		</a>
 	</div>
+	<a
+		href="https://www.linkedin.com/in/dominik-garstenauer/"
+		target="_blank"
+		class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+	>
+		<div
+			class="absolute top-0 right-full w-full h-full bg-gradient-to-r from-violet-400 to-violet-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200"
+		/>
+		<h4 class="relative z-9">Get in touch</h4>
+	</a>
 </header>
