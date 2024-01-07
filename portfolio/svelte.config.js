032ccbb7095c8705d2a66483f1cf9bddb980e9d5
dominikg-tum <dgarstenauer0@gmail.com>
2024-01-07 13:06:32 +0100

    Added /docs folder

--- portfolio/svelte.config.js
+++ portfolio/svelte.config.js
@@ -11,8 +11,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// default options are shown
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: null
 		}),
 		paths: {
